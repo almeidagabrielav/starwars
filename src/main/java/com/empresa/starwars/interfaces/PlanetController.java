9d7fbdd4ee9e848c563e2c476ac6a07f94cd0e98
Gabriela Almeida <almeidagabrielav@gmail.com>
2020-06-27 00:16:29 -0300

    Criação dos endpoints da api

--- src/main/java/com/empresa/starwars/interfaces/PlanetController.java
+++ src/main/java/com/empresa/starwars/interfaces/PlanetController.java
@@ -6,9 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -29,8 +27,34 @@ public class PlanetController {
                 .orElse(ResponseEntity.status(418).build());
     }
 
-    @GetMapping( value = "/planets/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity getById(@PathVariable(value = "id") int id){
+    @PostMapping( value = "/planets", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity createPlanet(@RequestBody Planet newPlanet){
+        List<Planet> planets  = planetService.createPlanet(newPlanet);
+
+        return Optional.ofNullable(planets)
+                .map(x -> ResponseEntity.ok().body(planets))
+                .orElse(ResponseEntity.status(418).build());
+    }
+
+    @PutMapping( value = "/planets/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity updatePlanet(@PathVariable(value = "id") int id, @RequestBody Planet planetDetails){
+        List<Planet> planets  = planetService.updatePlanet(id,planetDetails);
+
+        return Optional.ofNullable(planets)
+                .map(x -> ResponseEntity.ok().body(planets))
+                .orElse(ResponseEntity.status(418).build());
+    }
+
+    @DeleteMapping( value = "/planets/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity deletePlanet(@PathVariable(value = "id") int id){
+
+        return Optional.ofNullable(id)
+                .map(x -> ResponseEntity.ok().body(id))
+                .orElse(ResponseEntity.status(418).build());
+    }
+
+    @GetMapping( value = "/planets", params = "id", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity getById(@RequestParam int id){
         Planet planet  = planetService.getById(id);
 
         return Optional.ofNullable(planet)
@@ -39,12 +63,12 @@ public class PlanetController {
     }
 
 
-    @GetMapping( value = "/planets/{name}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity getByName(@PathVariable(value = "name") String name){
-        List<Planet> planets  = planetService.getByName(name);
+    @GetMapping( value = "/planets", params="name", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity getByName(@RequestParam String name){
+        Planet planet  = planetService.getByName(name);
 
-        return Optional.ofNullable(planets)
-                .map(x -> ResponseEntity.ok().body(planets))
+        return Optional.ofNullable(planet)
+                .map(x -> ResponseEntity.ok().body(planet))
                 .orElse(ResponseEntity.status(418).build());
     }
 }
